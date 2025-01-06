ff136c494b8f7519ce32057472253b250c36c2ed
JolyRed <jolywolf.22341@mail.ru>
2025-01-06 20:01:12 +0100

    postgres

--- module19/module19/settings.py
+++ module19/module19/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'module19.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "shop",
+        "USER": "postgres",
+        "PASSWORD": "z123hd8x",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
